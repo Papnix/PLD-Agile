78b510003fb9229f518caad09d27baef75ceb404
fmacerouss <fmacerouss@IF501-204-08.insa-lyon.fr>
2016-11-18 09:15:12 +0100

    modif Delivery et Warehouse

--- src/main/java/model/Delivery.java
+++ src/main/java/model/Delivery.java
@@ -2,17 +2,50 @@ package model;
 
 import java.util.Date;
 
-public class Delivery extends Waypoint
+public class Delivery
 {
 	
+	private int associatedWaypointId;
 	private int duration;
 	private Date timeRangeStart;
 	private Date timeRangeEnd;
 	
-	public Delivery(int pId, int pX, int pY) {
-		super(pId, pX, pY);
-		// TODO Auto-generated constructor stub
+	public Delivery(int associatedWaypointId, int duration, Date timeRangeStart, Date timeRangeEnd) {
+		super();
+		this.associatedWaypointId = associatedWaypointId;
+		this.duration = duration;
+		this.timeRangeStart = timeRangeStart;
+		this.timeRangeEnd = timeRangeEnd;
 	}
+	
+	public int getAssociatedWaypointId() {
+		return associatedWaypointId;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public Date getTimeRangeStart() {
+		return timeRangeStart;
+	}
+
+	public void setTimeRangeStart(Date timeRangeStart) {
+		this.timeRangeStart = timeRangeStart;
+	}
+
+	public Date getTimeRangeEnd() {
+		return timeRangeEnd;
+	}
+
+	public void setTimeRangeEnd(Date timeRangeEnd) {
+		this.timeRangeEnd = timeRangeEnd;
+	}
+
 	
 	
 	
--- src/main/java/model/Warehouse.java
+++ src/main/java/model/Warehouse.java
@@ -1,11 +1,27 @@
 package model;
 
-public class Warehouse extends Waypoint{
+import java.util.Date;
 
+public class Warehouse{
+
+	private int associatedWaypointId;
+	private Date startTime;
 	
-	public Warehouse(int pId, int pX, int pY) {
-		super(pId, pX, pY);
-		// TODO Auto-generated constructor stub
+	public Warehouse(int associatedWaypointId, Date startTime) {
+		this.associatedWaypointId = associatedWaypointId;
+		this.startTime = startTime;
 	}
 
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public int getAssociatedWaypointId() {
+		return associatedWaypointId;
+	}
+	
 }
