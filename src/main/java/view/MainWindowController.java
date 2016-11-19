2a754187c772b32d31cfb658477757c89db03149
Papnix <papnix@gmail.com>
2016-11-19 17:51:50 +0100

    TCK3 Amélioration base GUI

--- src/main/java/view/MainWindowController.java
+++ src/main/java/view/MainWindowController.java
@@ -1,28 +1,53 @@
 package view;
 
-/**
- * Sample Skeleton for "simple.fxml" Controller Class
- * Use copy/paste to copy paste this code into your favorite IDE
- **/
-
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 
 
-public class MainWindowController implements Initializable {
+public class MainWindowController implements Initializable{
 
-    @FXML //  fx:id="myButton"
-    private Button myButton; // Value injected by FXMLLoader
-
+    @FXML
+    private Button loadDeliveryButton;
+    @FXML
+    private Button loadMapButton;
+    @FXML
+    private MenuItem menuLoadMap;
+    @FXML
+    private MenuItem menuLoadDelivery;
 
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-        //assert myButton != null : "fx:id=\"myButton\" was not injected: check your FXML file 'simple.fxml'.";
-
-        // initialize your logic here: all @FXML variables will have been injected
-
+        assert loadDeliveryButton != null : "fx:id=\"loadDeliveryButton\" was not injected: check your FXML file 'simple.fxml'.";
+        loadDeliveryButton.setOnAction((event) -> {
+        	HandleLoadDelivery();
+        });
+                
+        assert loadMapButton != null : "fx:id=\"loadMapButton\" was not injected: check your FXML file 'simple.fxml'.";
+        loadMapButton.setOnAction((event) -> {
+        	HandleLoadMap();
+        });
+        
+        assert menuLoadMap != null : "fx:id=\"menuLoadMap\" was not injected: check your FXML file 'simple.fxml'.";
+        menuLoadMap.setOnAction((event) -> {
+        	HandleLoadMap();
+        });
+        
+        assert menuLoadDelivery != null : "fx:id=\"menuLoadDelivery\" was not injected: check your FXML file 'simple.fxml'.";
+        menuLoadDelivery.setOnAction((event) -> {
+        	HandleLoadDelivery();
+        });
+    }
+    
+    private void HandleLoadDelivery() {
+    	System.out.println("Chargement demande livraison");
+    }
+    private void HandleLoadMap() {
+	
+    	System.out.println("Chargement Map");
     }
 
 }
