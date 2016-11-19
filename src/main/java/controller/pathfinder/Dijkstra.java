812e0fbb8b8c92b524ee5d047fb02535a5f19437
Folsark <morbo38@gmail.com>
2016-11-19 19:32:35 +0100

    Import d'un algorithme de base de Dijkstrat a adapté en fonction de ce que l'on veut

--- src/main/java/controller/pathfinder/Dijkstra.java
+++ src/main/java/controller/pathfinder/Dijkstra.java
@@ -1,7 +1,9 @@
 package controller.pathfinder;
 
-import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
 import java.util.TreeMap;
 
 import model.Map;
@@ -9,20 +11,106 @@ import model.Section;
 
 public class Dijkstra
 {	
+	
+	private static Map map;
+	private Set<Integer> settledNodes;
+    private Set<Integer> unSettledNodes;
+    private TreeMap<Integer, Integer> predecessors;
+    private TreeMap<Integer, Double> cost;
+	
+    
+    /*
+     * This method returns the path from the source to the selected target and
+     * NULL if no path exists
+     */
+    public LinkedList<Integer> getPath(Integer target) {
+            map = Map.getInstance();
+    		LinkedList<Integer> path = new LinkedList<Integer>();
+            Integer step = target;
+            // check if a path exists
+            if (predecessors.get(step) == null) {
+                    return null;
+            }
+            path.add(step);
+            while (predecessors.get(step) != null) {
+                    step = predecessors.get(step);
+                    path.add(step);
+            }
+            // Put it into the correct order
+            Collections.reverse(path);
+            return path;
+    }
+    
+    
+    public void execute(int source) {
+        settledNodes = new HashSet<Integer>();
+        unSettledNodes = new HashSet<Integer>();
+        cost = new TreeMap<Integer, Double>();
+        predecessors = new TreeMap<Integer, Integer>();
+        cost.put(source, 0.0);
+        unSettledNodes.add(source);
+        while (unSettledNodes.size() > 0) {
+        		Integer node = getMinimum(unSettledNodes);
+                settledNodes.add(node);
+                unSettledNodes.remove(node);
+                findMinimalCost(node);
+        }
+}
+    
 	public static int[] getSuccessors(int idOrigin)
 	{
-		Map map = Map.getInstance();
-		
 		Object[] sections = map.getSections().get(idOrigin).values().toArray();
-		
 		int[] successors = new int[sections.length];
-		
 		for (int i = 0; i < successors.length; i++)
 		{
 			successors[i] = ((Section)sections[i]).getDestination().getId();
 		}
 		
 		return successors;
+	}
+	
+	public static double calculateCost(int idOrigin, int idDestination)
+	{
+		Map map = Map.getInstance();
+		Section section = map.getSection(idOrigin, idDestination);
+		
+		return section.getLength()/section.getSpeed();
+	}
+	
+	private void findMinimalCost(int idOrigin) {
+		int[] successors = getSuccessors(idOrigin);
+	    for (int target : successors) {
+	        if (getShortestDistance(target) > getShortestDistance(idOrigin) + calculateCost(idOrigin, target)) {
+	        		cost.put(target, getShortestDistance(idOrigin) + calculateCost(idOrigin, target));
+	                predecessors.put(target, idOrigin);
+	                unSettledNodes.add(target);
+	        }
+	    }
+	}
+	
+	// 
+	private int getMinimum(Set<Integer> vertexes) {
+		Integer minimum = null;
+        for (Integer vertex : vertexes) {
+                if (minimum == null) {
+                        minimum = vertex;
+                } else {
+                        if (getShortestDistance(vertex) < getShortestDistance(minimum)) {
+                                minimum = vertex;
+                        }
+                }
+        }
+        return minimum;
+}
+	
+	// trouve le plus petit cout allant au noeud idDestination dans le tableau de cout.
+	private double getShortestDistance(int idDestination) {
+        Double d = cost.get(idDestination);
+        if (d == null) {
+                return Double.MAX_VALUE;
+        } else {
+                return d;
+        }
 	}
 	
 }
--- src/main/java/test/CalculatorTest.java
+++ src/main/java/test/CalculatorTest.java
@@ -48,23 +48,63 @@ public class CalculatorTest {
 	  int [] result = {1,6,7}; 
 	  int [] successors = Dijkstra.getSuccessors(2);
 	  Arrays.sort(successors);
-	  Arrays.sort(result);
-	  //assertEquals(result, successors);
 	  Assert.assertArrayEquals(result, successors);
 	  
 	//recupere les successeurs du noeud d'id = 1, classé par id croissant.
 	  int [] result1 = {2,6}; 
 	  successors = Dijkstra.getSuccessors(1);
 	  Arrays.sort(successors);
-	  Arrays.sort(result1);
-	  //assertEquals(result1, successors);
 	  Assert.assertArrayEquals(result1, successors);
 	  
 	//recupere les successeurs du noeud d'id = 7, classé par id croissant.
 	  successors = Dijkstra.getSuccessors(7);
 	  Arrays.sort(successors);
-	  //assertEquals(result1, successors);
 	  Assert.assertArrayEquals(result1, successors);
+  }
+  
+  @Test
+  public void evaluateCost()
+  {
+	  try {	
+		  // ouverture de la map de test (créé a cette effet)
+			XMLDeserializer.loadMap("src/main/resources/archivePLD2016/plan2x2.xml");
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XMLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	  
+	  //respectivemet sommet s1, s2, s3;
+	  int s1, s2, s3;
+	  s1 = 2;
+	  s2 = 6;
+	  s3 = 7;
+	  double costS1S2Estimated, costS2S1Estimated, costS1S3Estimated, costS2S3Estimated;
+	  
+	  //Calcul et test cout de l'"arrete" s1-s2.
+	  costS1S2Estimated = 9082/29;
+	  Assert.assertEquals(costS1S2Estimated,Dijkstra.calculateCost(s1,s2), 0.0001);
+	  
+	//Calcul et test cout de l'"arrete" s2-s1.
+	  costS2S1Estimated = 9082/26;
+	  Assert.assertEquals(costS2S1Estimated,Dijkstra.calculateCost(s2,s1), 0.0001);
+	  
+	//Calcul et test cout de l'"arrete" s1-s3.
+	  costS1S3Estimated = 10257/38;
+	  Assert.assertEquals(costS1S3Estimated,Dijkstra.calculateCost(s1,s3), 0.0001);
+	  
+	//Calcul et test cout de l'"arrete" s2-s3.
+	  costS2S3Estimated = 5440/43;
+	  Assert.assertEquals(costS2S3Estimated,Dijkstra.calculateCost(s2,s3), 0.0001);
+	  
   }
   
 //permet d'evaluer la methode getSuccessor de l'algorithme de Dijkstra (premier pas vers le calcul de la tourné)
