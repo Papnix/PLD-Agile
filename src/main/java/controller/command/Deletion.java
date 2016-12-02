57c46eeda1bbabec1d0d022c301f0125fca7c3f0
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2016-12-02 11:05:52 +0100

    Ajout suppression checkpoint

--- src/main/java/controller/command/Deletion.java
+++ src/main/java/controller/command/Deletion.java
@@ -13,14 +13,14 @@ public class Deletion extends Command {
     }
 
     public Round doCommand(Checkpoint checkpoint) {
-        /*List<DeliveryTime> arrivalTimes = round.getArrivalTimes();
-        for(int i = 0; i<arrivalTimes.size(); i++) {
-            if(arrivalTimes.get(i).getCheckpoint().getId() == this.deliveryTime.getCheckpoint().getId()) {
-                index = i;
-                round.getArrivalTimes().remove(i);
+        this.modifiedRound = new Round(this.previousRound);
+        List<DeliveryTime> arrivalTimes = this.modifiedRound.getArrivalTimes();
+        for (int i = 0; i < arrivalTimes.size(); i++) {
+            if (arrivalTimes.get(i).getCheckpoint().getId() == checkpoint.getId()) {
+                this.modifiedRound.getArrivalTimes().remove(i);
                 break;
             }
-        }*/
+        }
         return this.modifiedRound;
     }
 }
