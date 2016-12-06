fd9c906913d594676797cd7e2b872a2086a0cdb6
unknown <fmacerouss@IF501-204-08.insa-lyon.fr>
2016-12-06 08:43:32 +0100

    retour review 1 ticket #22

--- src/main/java/controller/tsp/TSP.java
+++ src/main/java/controller/tsp/TSP.java
@@ -6,29 +6,42 @@ import model.Checkpoint;
 import model.DeliveryTime;
 
 public interface TSP {
-		
+
 	/**
-	 * @return true si chercheSolution() s'est terminee parce que la limite de temps avait ete atteinte, avant d'avoir pu explorer tout l'espace de recherche,
+	 * @return true si chercheSolution() s'est terminee parce que la limite de
+	 *         temps avait ete atteinte, avant d'avoir pu explorer tout l'espace
+	 *         de recherche,
 	 */
-	public Boolean getTempsLimiteAtteint();
-	
+	public Boolean getLimitTimeReached();
+
 	/**
-	 * Cherche un circuit de duree minimale passant par chaque sommet (compris entre 0 et nbSommets-1)
-	 * @param tpsLimite : limite (en millisecondes) sur le temps d'execution de chercheSolution
-	 * @param nbSommets : nombre de sommets du graphe
-	 * @param cout : cout[i][j] = duree pour aller de i a j, avec 0 <= i < nbSommets et 0 <= j < nbSommets
-	 * @param duree : duree[i] = duree pour visiter le sommet i, avec 0 <= i < nbSommets
+	 * Cherche un circuit de duree minimale passant par chaque sommet (compris
+	 * entre 0 et nbSommets-1)
+	 * 
+	 * @param limitTime
+	 *            : limite (en millisecondes) sur le temps d'execution de
+	 *            chercheSolution
+	 * @param numOfVertices
+	 *            : nombre de sommets du graphe
+	 * @param cout
+	 *            : cout[i][j] = duree pour aller de i a j, avec 0 <= i <
+	 *            nbSommets et 0 <= j < nbSommets
+	 * @param duration
+	 *            : duree[i] = duree pour visiter le sommet i, avec 0 <= i <
+	 *            nbSommets
 	 */
-	public int chercheSolution(int tpsLimite, int nbSommets, int[][] cout, int[] duree, List<Checkpoint> checkpointList);
-	
+	public int findSolution(int limitTime, int numOfVertices, int[][] cout, int[] duration,
+			List<Checkpoint> checkpointList);
+
 	/**
 	 * @param i
-	 * @return le sommet visite en i-eme position dans la solution calculee par chercheSolution
+	 * @return le sommet visite en i-eme position dans la solution calculee par
+	 *         chercheSolution
 	 */
-	public DeliveryTime getMeilleureSolution(int i, int j);
-	
-	/** 
+	public DeliveryTime getBestSolution(int i, int j);
+
+	/**
 	 * @return la duree de la solution calculee par chercheSolution
 	 */
-	public long getCoutMeilleureSolution();
+	public long getBestSolutionCost();
 }
--- src/main/java/controller/tsp/TemplateTSP.java
+++ src/main/java/controller/tsp/TemplateTSP.java
@@ -12,10 +12,10 @@ import model.DeliveryTime;
 
 public abstract class TemplateTSP implements TSP {
 
-	private DeliveryTime[] meilleureSolution;
-	private long coutMeilleureSolution = 0;
+	private DeliveryTime[] bestSolution;
+	private long BestSolutionCost = 0;
 	private List<DeliveryTime[]> roundList;
-	private Boolean tempsLimiteAtteint;
+	private Boolean limiteTimeReached;
 	private HashMap<Integer, Integer> indexValues;
 
 	private DeliveryTime[] copyOf(DeliveryTime[] dt) {
@@ -64,38 +64,37 @@ public abstract class TemplateTSP implements TSP {
 		}
 	}
 
-	public Boolean getTempsLimiteAtteint() {
-		return tempsLimiteAtteint;
+	public Boolean getLimitTimeReached() {
+		return limiteTimeReached;
 	}
 
-	public int chercheSolution(int tpsLimite, int nbSommets, int[][] cout, int[] duree,
-			List<Checkpoint> checkpointList) {
-		tempsLimiteAtteint = false;
-		coutMeilleureSolution = Long.MAX_VALUE;
-		meilleureSolution = new DeliveryTime[nbSommets + 1];
+	public int findSolution(int tpsLimite, int nbSommets, int[][] cout, int[] duree, List<Checkpoint> checkpointList) {
+		limiteTimeReached = false;
+		BestSolutionCost = Long.MAX_VALUE;
+		bestSolution = new DeliveryTime[nbSommets + 1];
 		roundList = new ArrayList<DeliveryTime[]>();
 		// buildIndex(checkpointList);
-		List<DeliveryTime> nonVus = new ArrayList<DeliveryTime>();
+		List<DeliveryTime> unseen = new ArrayList<DeliveryTime>();
 		for (Checkpoint checkpoint : checkpointList) {
-			nonVus.add(new DeliveryTime(checkpoint, null, null, 0));
+			unseen.add(new DeliveryTime(checkpoint, null, null, 0));
 		}
-		buildIndex(nonVus);
-		nonVus.remove(0);
-		ArrayList<DeliveryTime> vus = new ArrayList<DeliveryTime>(checkpointList.size());
-		vus.add(new DeliveryTime(checkpointList.get(0), null, checkpointList.get(0).getTimeRangeStart(), 0)); // le
+		buildIndex(unseen);
+		unseen.remove(0);
+		ArrayList<DeliveryTime> seen = new ArrayList<DeliveryTime>(checkpointList.size());
+		seen.add(new DeliveryTime(checkpointList.get(0), null, checkpointList.get(0).getTimeRangeStart(), 0)); // le
 																												// premier
 																												// sommet
 																												// visite
 																												// est
 																												// 0
-		branchAndBound(vus.get(0), nonVus, vus, 0, cout, duree, checkpointList.get(0).getTimeRangeStart().getTime(),
+		branchAndBound(seen.get(0), unseen, seen, 0, cout, duree, checkpointList.get(0).getTimeRangeStart().getTime(),
 				tpsLimite);
 		completeRound(cout);
 		return roundList.size();
 	}
 
-	public DeliveryTime getMeilleureSolution(int i, int j) {
-		if ((meilleureSolution == null) || (j < 0) || (j >= meilleureSolution.length))
+	public DeliveryTime getBestSolution(int i, int j) {
+		if ((bestSolution == null) || (j < 0) || (j >= bestSolution.length))
 			return null;
 		return getBestRound(i)[j];
 	}
@@ -115,114 +114,114 @@ public abstract class TemplateTSP implements TSP {
 		}
 	}
 
-	public long getCoutMeilleureSolution() {
-		return coutMeilleureSolution;
+	public long getBestSolutionCost() {
+		return BestSolutionCost;
 	}
 
 	/**
 	 * Methode devant etre redefinie par les sous-classes de TemplateTSP
 	 * 
-	 * @param sommetcrt
-	 * @param nonVus
+	 * @param currentVertice
+	 * @param unseen
 	 *            : tableau des sommets restant a visiter
-	 * @param cout
+	 * @param cost
 	 *            : cout[i][j] = duree pour aller de i a j, avec 0 <= i <
 	 *            nbSommets et 0 <= j < nbSommets
-	 * @param duree
+	 * @param duration
 	 *            : duree[i] = duree pour visiter le sommet i, avec 0 <= i <
 	 *            nbSommets
 	 * @return une borne inferieure du cout des permutations commencant par
 	 *         sommetCourant, contenant chaque sommet de nonVus exactement une
 	 *         fois et terminant par le sommet 0
 	 */
-	protected abstract int bound(Checkpoint sommetcrt, List<DeliveryTime> nonVus, int[][] cout, int[] duree);
+	protected abstract int bound(Checkpoint currentVertice, List<DeliveryTime> unseen, int[][] cost, int[] duration);
 
 	/**
 	 * Methode devant etre redefinie par les sous-classes de TemplateTSP
 	 * 
-	 * @param sommetcrt
-	 * @param nonVus
+	 * @param currentVertice
+	 * @param unseen
 	 *            : tableau des sommets restant a visiter
-	 * @param cout
+	 * @param cost
 	 *            : cout[i][j] = duree pour aller de i a j, avec 0 <= i <
 	 *            nbSommets et 0 <= j < nbSommets
-	 * @param duree
+	 * @param duration
 	 *            : duree[i] = duree pour visiter le sommet i, avec 0 <= i <
 	 *            nbSommets
 	 * @return un iterateur permettant d'iterer sur tous les sommets de nonVus
 	 */
-	protected abstract Iterator<DeliveryTime> iterator(DeliveryTime sommetcrt, List<DeliveryTime> nonVus, int[][] cout,
-			int[] duree);
+	protected abstract Iterator<DeliveryTime> iterator(DeliveryTime currentVertice, List<DeliveryTime> unseen,
+			int[][] cost, int[] duration);
 
 	/**
 	 * Methode definissant le patron (template) d'une resolution par separation
 	 * et evaluation (branch and bound) du TSP
 	 * 
-	 * @param sommetCourant
+	 * @param currentVertice
 	 *            le dernier sommet visite
-	 * @param nonVus
+	 * @param unseen
 	 *            la liste des sommets qui n'ont pas encore ete visites
-	 * @param vus
+	 * @param seen
 	 *            la liste des sommets visites (y compris sommetCrt)
-	 * @param coutVus
+	 * @param seenCost
 	 *            la somme des couts des arcs du chemin passant par tous les
 	 *            sommets de vus + la somme des duree des sommets de vus
-	 * @param cout
+	 * @param cost
 	 *            : cout[i][j] = duree pour aller de i a j, avec 0 <= i <
 	 *            nbSommets et 0 <= j < nbSommets
-	 * @param duree
+	 * @param duration
 	 *            : duree[i] = duree pour visiter le sommet i, avec 0 <= i <
 	 *            nbSommets
-	 * @param tpsDebut
+	 * @param startTime
 	 *            : moment ou la resolution a commence
-	 * @param tpsLimite
+	 * @param timeLimit
 	 *            : limite de temps pour la resolution
 	 */
-	void branchAndBound(DeliveryTime sommetCourant, List<DeliveryTime> nonVus, ArrayList<DeliveryTime> vus, long coutVus,
-			int[][] cout, int[] duree, long tpsDebut, int tpsLimite) {
+	void branchAndBound(DeliveryTime currentVertice, List<DeliveryTime> unseen, ArrayList<DeliveryTime> seen,
+			long seenCost, int[][] cost, int[] duration, long startTime, int timeLimit) {
 
-		Date arrivalDate = new Date(coutVus + tpsDebut);
-		long waitingTime = validTimeRange(sommetCourant.getCheckpoint(), arrivalDate);
+		Date arrivalDate = new Date(seenCost + startTime);
+		long waitingTime = validTimeRange(currentVertice.getCheckpoint(), arrivalDate);
 		if (waitingTime != -1) {
-			Date departureTime = new Date(
-					arrivalDate.getTime() + waitingTime + duree[indexValues.get(sommetCourant.getCheckpoint().getId())]);
-			sommetCourant.setArrivalTime(arrivalDate);
-			sommetCourant.setDepartureTime(departureTime);
-			sommetCourant.setWaitingTime(waitingTime);
-			if (nonVus.size() == 0) { // tous les sommets ont ete visites
-				coutVus += cout[indexValues.get(sommetCourant.getCheckpoint().getId())][0];
-				if (coutVus < coutMeilleureSolution) { // on a trouve une
-														// solution meilleure
-														// que meilleureSolution
+			Date departureTime = new Date(arrivalDate.getTime() + waitingTime
+					+ duration[indexValues.get(currentVertice.getCheckpoint().getId())]);
+			currentVertice.setArrivalTime(arrivalDate);
+			currentVertice.setDepartureTime(departureTime);
+			currentVertice.setWaitingTime(waitingTime);
+			if (unseen.size() == 0) { // tous les sommets ont ete visites
+				seenCost += cost[indexValues.get(currentVertice.getCheckpoint().getId())][0];
+				if (seenCost < BestSolutionCost) { // on a trouve une
+													// solution meilleure
+													// que meilleureSolution
 					roundList.clear();
-					vus.toArray(meilleureSolution);
-					roundList.add(copyOf(meilleureSolution));
-					coutMeilleureSolution = coutVus;
-				} else if (coutVus == coutMeilleureSolution) {
-					vus.toArray(meilleureSolution);
-					if (!contains(meilleureSolution)) {
-						roundList.add(copyOf(meilleureSolution));
+					seen.toArray(bestSolution);
+					roundList.add(copyOf(bestSolution));
+					BestSolutionCost = seenCost;
+				} else if (seenCost == BestSolutionCost) {
+					seen.toArray(bestSolution);
+					if (!contains(bestSolution)) {
+						roundList.add(copyOf(bestSolution));
 					}
 				}
-			} else if (coutVus + waitingTime
-					+ bound(sommetCourant.getCheckpoint(), nonVus, cout, duree) < coutMeilleureSolution) {
-				Iterator<DeliveryTime> it = iterator(sommetCourant, nonVus, cout, duree);
+			} else if (seenCost + waitingTime
+					+ bound(currentVertice.getCheckpoint(), unseen, cost, duration) < BestSolutionCost) {
+				Iterator<DeliveryTime> it = iterator(currentVertice, unseen, cost, duration);
 				while (it.hasNext()) {
-					DeliveryTime prochainSommet = it.next();
-					vus.add(prochainSommet);
-					nonVus.remove(prochainSommet);
-					branchAndBound(prochainSommet, nonVus, vus,
+					DeliveryTime nextVertice = it.next();
+					seen.add(nextVertice);
+					unseen.remove(nextVertice);
+					branchAndBound(nextVertice, unseen, seen,
 							departureTime.getTime()
-									+ cout[indexValues.get(sommetCourant.getCheckpoint().getId())][indexValues
-											.get(prochainSommet.getCheckpoint().getId())]
-									- tpsDebut,
-							cout, duree, tpsDebut, tpsLimite);
+									+ cost[indexValues.get(currentVertice.getCheckpoint().getId())][indexValues
+											.get(nextVertice.getCheckpoint().getId())]
+									- startTime,
+							cost, duration, startTime, timeLimit);
 					// coutVus + waitingTime +
 					// cout[indexValues.get(sommetCrt.getCheckpoint().getId())][indexValues.get(prochainSommet.getCheckpoint().getId())]
 					// +
 					// duree[indexValues.get(prochainSommet.getCheckpoint().getId())]
-					vus.remove(prochainSommet);
-					nonVus.add(prochainSommet);
+					seen.remove(nextVertice);
+					unseen.add(nextVertice);
 				}
 			}
 		}
