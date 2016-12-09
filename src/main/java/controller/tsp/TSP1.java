7372ebd44cc30dc5280e06f4aef19af91ad16303
Alexandre <Alexandre@DESKTOP-ALEX.insa-lyon.fr>
2016-12-09 17:03:35 +0100

    Ajout du TSP le plus efficace

--- src/main/java/controller/tsp/TSP1.java
+++ src/main/java/controller/tsp/TSP1.java
@@ -10,7 +10,7 @@ import model.DeliveryTime;
 public class TSP1 extends TemplateTSP {
 
 	@Override
-	protected int bound(Checkpoint currentVertice, List<DeliveryTime> unseen, int[][] cost, int[] duration) {
+	protected int bound(Checkpoint currentVertex, List<DeliveryTime> unseen, int[][] cost, int[] duration) {
 		
 		int bound = 0;
 		
@@ -20,18 +20,23 @@ public class TSP1 extends TemplateTSP {
 			indicesToConsider.add(indexValues.get(dt1.getCheckpoint().getId()));
 		}
 		
-		for (Integer index : indicesToConsider) {
+		// From current vertex to unseen ones
+		
+		int cur_index = indexValues.get(currentVertex.getId());
+		
+		bound += getMinimumCost(cur_index, cost[cur_index], indicesToConsider) + duration[cur_index];
+		
+		// In between unseen vertices
+		
+		for (int i = 0; i < indicesToConsider.size() - 1; i++) {
 			
-			int[] outCost = cost[index];
-			
-			int min = getMinimum(index, outCost, indicesToConsider);
-			
-			if (outCost.length > 0) {
-				bound += min;
-			}
-			
-			bound += duration[index];
+			int index = indicesToConsider.get(i);
+			bound += getMinCostToVertex(index, cost, indicesToConsider) + duration[index];
 		}
+		
+		// To return to warehouse
+		
+		bound += getMinCostToWarehouse(cost, indicesToConsider);
 		
 		return bound;
 	}
@@ -50,8 +55,7 @@ public class TSP1 extends TemplateTSP {
 	 * @param indicesToConsider list of the nodes to consider to compute the lower bound
 	 * @return the minimum edge leaving the current node
 	 */
-	private int getMinimum(int cur_index, int[] array, List<Integer> indicesToConsider)
-	{
+	private int getMinimumCost(int cur_index, int[] array, List<Integer> indicesToConsider){
 		int min = Integer.MAX_VALUE;
 		
 		for (int i = 0; i < array.length; i++) {
@@ -59,6 +63,43 @@ public class TSP1 extends TemplateTSP {
 				if (indicesToConsider.contains(i)) {
 					min = array[i];
 				}
+			}
+		}
+		
+		return min;
+	}
+	
+	/**
+	 * This method returns the cost of the minimum path from the vertices contained in indicesToConsider, to the warehouse.
+	 * @param cost the general cost matrix
+	 * @param indicesToConsider the indices of the vertices that should be considered in the computation of the minimum.
+	 * @return the cost of the minimum path to the warehouse.
+	 */
+	private int getMinCostToWarehouse(int[][] cost, List<Integer> indicesToConsider) {
+		int min = Integer.MAX_VALUE;
+		
+		for (Integer index : indicesToConsider) {
+			if (cost[index][0] < min) {
+				min = cost[index][0];
+			}
+		}
+		
+		return min;
+	}
+	
+	/**
+	 * This method gives the minimum cost to a particular vertex, from any vertex contained in indicesToConsider.
+	 * @param vertex the destination vertex
+	 * @param cost the general cost matrix
+	 * @param indicesToConsider the indices of the vertices that should be considered in the computation of the minimum.
+	 * @return
+	 */
+	private int getMinCostToVertex(int vertex, int[][] cost, List<Integer> indicesToConsider) {
+		int min = Integer.MAX_VALUE;
+		
+		for (Integer i : indicesToConsider) {
+			if (cost[i][vertex] < min && vertex != i) {
+				min = cost[i][vertex];
 			}
 		}
 		
