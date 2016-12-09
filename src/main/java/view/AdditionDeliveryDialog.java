ab990f763e33978a2383f5b02aa3ff45e2b0e418
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2016-12-09 09:14:36 +0100

    Gestion inputs vides pour les dates (revient a null)

--- src/main/java/view/AdditionDeliveryDialog.java
+++ src/main/java/view/AdditionDeliveryDialog.java
@@ -17,43 +17,43 @@ import model.Checkpoint;
 
 
 public class AdditionDeliveryDialog extends ModificationDialog {
-	
+
 	private TextField idWaypointField;
 	private TextField startDateField;
 	private TextField endDateField;
 	private TextField durationField;
-	private SimpleDateFormat timingFormat = new SimpleDateFormat("HH:mm:ss", Locale.FRANCE);
+	private SimpleDateFormat timingFormat = new SimpleDateFormat("HH:mm", Locale.FRANCE);
 
 	public AdditionDeliveryDialog(Controller controller) {
 		super(controller);
-		setHeaderText("Vous �tes sur le point de modifier une plage horraire d'une livraison � la tourn�e courante\n" 
-				+ "S�lectionez la livraison � modifier puis indiquez la plage horaire.");
-		
+		setHeaderText("Vous etes sur le point d'ajouter une livraison dans la tournee courante\n"
+				+ "Indiquez les information de la nouvelle livraison.");
+
 		idWaypointField = new TextField();
 		startDateField = new TextField();
 		endDateField = new TextField();
 		durationField = new TextField();
-		
+
 		grid.add(new Label("Id du lieu : "), 2, 1);
 		grid.add(idWaypointField, 2, 2);
-		grid.add(new Label("Date d'arriv� : "), 2, 3);
+		grid.add(new Label("Heure d'ouverture (HH:mm) : "), 2, 3);
 		grid.add(startDateField, 2, 4);
-		grid.add(new Label("Date de d�part : "), 2, 5);
+		grid.add(new Label("Heure de fermeture (HH:mm) : "), 2, 5);
 		grid.add(endDateField, 2, 6);
-		grid.add(new Label("Dur�e de la livraison : "), 2, 7);
+		grid.add(new Label("Duree de la livraison (en secondes) : "), 2, 7);
 		grid.add(durationField, 2, 8);
-		
+
 		deliveryCombo.setVisible(false);
 		labelCombo.setVisible(false);
 		Node loginButton = getDialogPane().lookupButton(buttonOk);
 		loginButton.setDisable(false);
 		defineOnCloseAction(controller);
-		
+
 	}
 
 	@Override
 	protected void onComboValueChanged() {
-		
+
 	}
 
 	@Override
@@ -67,12 +67,12 @@ public class AdditionDeliveryDialog extends ModificationDialog {
 					try {
 						int id = Integer.parseInt(idWaypointField.getText());
 						int duration = Integer.parseInt(idWaypointField.getText());
-						
-						start = timingFormat.parse(startDateField.getText());
-						end = timingFormat.parse(endDateField.getText());
-						
+
+						start = startDateField.getText().equals("") ? null : timingFormat.parse(startDateField.getText());
+						end = endDateField.getText().equals("") ? null : timingFormat.parse(endDateField.getText());
+
 			        	controller.addCheckpoint(new Checkpoint( controller.getCurrentMap().getWaypoint(id), duration, start, end));
-						
+
 					} catch (ParseException e) {
 						ErrorDisplayer.displayWarningMessageBox("Mauvais format de date");
 					}
@@ -80,7 +80,7 @@ public class AdditionDeliveryDialog extends ModificationDialog {
 		        return null;
 		    }
 		});
-		
+
 	}
 
 }
